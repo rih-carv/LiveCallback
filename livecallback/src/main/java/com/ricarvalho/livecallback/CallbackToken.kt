1c5c084935e8875ce00fb9b36d672bcfa81d44ae
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-08-23 18:28:55 -0300

    Input only CallbackToken implementation

--- livecallback/src/main/java/com/ricarvalho/livecallback/CallbackToken.kt
+++ livecallback/src/main/java/com/ricarvalho/livecallback/CallbackToken.kt
@@ -1,5 +1,6 @@
 package com.ricarvalho.livecallback
 
+typealias InputCallbackToken<I> = CallbackToken<I, Unit>
 typealias OutputCallbackToken<O> = CallbackToken<Void, O>
 
 @kotlin.jvm.JvmInline
@@ -7,6 +8,8 @@ value class CallbackToken<I, O> private constructor(val token: String) {
     internal constructor(callback: (I) -> O) : this(callback.token)
 
     companion object {
+        internal fun <I> input(callback: (I) -> Unit) = InputCallbackToken<I>(callback.token)
+
         internal fun <O> output(callback: () -> O) = OutputCallbackToken<O>(callback.token)
 
         private val Any.token get() = this::class.java.name
--- livecallback/src/test/java/com/ricarvalho/livecallback/CallbackTokenTest.kt
+++ livecallback/src/test/java/com/ricarvalho/livecallback/CallbackTokenTest.kt
@@ -37,6 +37,39 @@ class CallbackTokenTest {
     }
     //endregion
 
+    //region InputCallbackToken
+    @Test
+    fun `input tokens of different callbacks shouldn't be equal`() {
+        val token1 = CallbackToken.input<String> {}
+        val token2 = CallbackToken.input<String> {}
+
+        assertNotEquals(token1, token2)
+    }
+
+    @Test
+    fun `input tokens of the same callback should be equal`() {
+        val callback: (String) -> Unit = {}
+        val token1 = CallbackToken.input(callback)
+        val token2 = CallbackToken.input(callback)
+
+        assertEquals(token1, token2)
+    }
+
+    @Test
+    fun `input tokens of different callback instances with the same origin should be equal`() {
+        val externalValueToForceNewLambdaInstance = ""
+        fun callback(): (String) -> Unit = { externalValueToForceNewLambdaInstance.run {} }
+
+        val callback1 = callback()
+        val callback2 = callback()
+        val token1 = CallbackToken.input(callback1)
+        val token2 = CallbackToken.input(callback2)
+
+        assertNotSame(callback1, callback2)
+        assertEquals(token1, token2)
+    }
+    //endregion
+
     //region OutputCallbackToken
     @Test
     fun `output tokens of different callbacks shouldn't be equal`() {
