a934457859c6c97d9c20171c882f61b5be0e2453
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-09-08 02:34:19 -0300

    Typealias documentation

--- livecallback/src/main/java/com/ricarvalho/livecallback/CallbackAlias.kt
+++ livecallback/src/main/java/com/ricarvalho/livecallback/CallbackAlias.kt
@@ -1,6 +1,28 @@
 package com.ricarvalho.livecallback
 
+/**
+ * A callback that receive a parameter and return a value.
+ *
+ * @param[I] The type of parameter the callback receives.
+ * @param[O] The type of return of the callback.
+ */
 typealias Callback<I, O> = (input: I) -> O
+
+/**
+ * A callback that receive a parameter and return no value.
+ *
+ * @param[I] The type of parameter the callback receives.
+ */
 typealias InputCallback<I> = Callback<I, Unit>
+
+/**
+ * A callback that receive no parameters and return a value.
+ *
+ * @param[O] The type of return of the callback.
+ */
 typealias OutputCallback<O> = () -> O
+
+/**
+ * A callback that receive no parameters and return no value.
+ */
 typealias SimpleCallback = OutputCallback<Unit>
--- livecallback/src/main/java/com/ricarvalho/livecallback/CallbackTokenAlias.kt
+++ livecallback/src/main/java/com/ricarvalho/livecallback/CallbackTokenAlias.kt
@@ -1,5 +1,20 @@
 package com.ricarvalho.livecallback
 
+/**
+ * A [CallbackToken] for callbacks that receive a parameter and return no value.
+ *
+ * @param[I] The type of parameter the callback receives.
+ */
 typealias InputCallbackToken<I> = CallbackToken<I, Unit>
+
+/**
+ * A [CallbackToken] for callbacks that receive no parameters and return a value.
+ *
+ * @param[O] The type of return of the callback.
+ */
 typealias OutputCallbackToken<O> = CallbackToken<Void, O>
+
+/**
+ * A [CallbackToken] for callbacks that receive no parameters and return no value.
+ */
 typealias SimpleCallbackToken = CallbackToken<Void, Unit>
