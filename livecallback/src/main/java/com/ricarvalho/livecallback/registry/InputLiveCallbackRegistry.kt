ff436e73c631bfef0790c587952c6dcbe121d22d
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-09-12 11:17:22 -0300

    LiveCallbackRegistries documentation

--- livecallback/src/main/java/com/ricarvalho/livecallback/registry/InputLiveCallbackRegistry.kt
+++ livecallback/src/main/java/com/ricarvalho/livecallback/registry/InputLiveCallbackRegistry.kt
@@ -1,3 +1,22 @@
 package com.ricarvalho.livecallback.registry
 
+import androidx.lifecycle.Lifecycle.State.DESTROYED
+import androidx.lifecycle.LifecycleOwner
+
+/**
+ * A lifecycle-aware registry of callbacks that correlates callbacks and its origins.
+ *
+ * This is expected to be used with callbacks that receive a parameter and return no value.
+ *
+ * It should respect the lifecycle of other app components, such as activities, fragments, or
+ * services. This awareness ensure the registered callbacks should be only invoked when appropriate.
+ *
+ * You can register a callback paired with an object that implements the [LifecycleOwner] interface.
+ * This relationship allows the callback to be released when the state of the lifecycle changes to
+ * [DESTROYED]. This is especially useful for activities and fragments because they can safely
+ * register callbacks and not worry about leaks: the callback and lifecycle are expected to be
+ * instantly released when their lifecycles are destroyed.
+ *
+ * @param[I] The type of parameter the callbacks receives.
+ */
 interface InputLiveCallbackRegistry<I> : LiveCallbackRegistry<I, Unit>
--- livecallback/src/main/java/com/ricarvalho/livecallback/registry/OutputLiveCallbackRegistry.kt
+++ livecallback/src/main/java/com/ricarvalho/livecallback/registry/OutputLiveCallbackRegistry.kt
@@ -1,10 +1,45 @@
 package com.ricarvalho.livecallback.registry
 
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.Lifecycle.State.DESTROYED
+import androidx.lifecycle.LifecycleOwner
+import com.ricarvalho.livecallback.CallbackToken
 import com.ricarvalho.livecallback.OutputCallback
 import com.ricarvalho.livecallback.OutputCallbackToken
 
+/**
+ * A lifecycle-aware registry of callbacks that correlates callbacks and its origins.
+ *
+ * This is expected to be used with callbacks that receive no parameters and return a value.
+ *
+ * It should respect the lifecycle of other app components, such as activities, fragments, or
+ * services. This awareness ensure the registered callbacks should be only invoked when appropriate.
+ *
+ * You can register a callback paired with an object that implements the [LifecycleOwner] interface.
+ * This relationship allows the callback to be released when the state of the lifecycle changes to
+ * [DESTROYED]. This is especially useful for activities and fragments because they can safely
+ * register callbacks and not worry about leaks: the callback and lifecycle are expected to be
+ * instantly released when their lifecycles are destroyed.
+ *
+ * @param[O] The type of return of the callbacks.
+ */
 interface OutputLiveCallbackRegistry<O> {
+
+    /**
+     * Registers [callback] paired with [lifecycle] to be invoked later.
+     *
+     * Concrete implementations should release them when [lifecycle] be [DESTROYED]. Also, they
+     * should allow callback to be invoked when appropriate, given that a [CallbackToken]
+     * corresponding to the one returned is provided.
+     *
+     * @param[lifecycle] The [Lifecycle] which [callback] will be paired to.
+     * @param[runWhileStopped] Indicates to invoke [callback] even when [lifecycle] is not active.
+     * @param[callback] The callback to be registered. Will be released when [lifecycle] be
+     * destroyed. May be invoked only when [lifecycle] is in an active state, or in whatever state
+     * until [lifecycle] be destroyed, according to [runWhileStopped]'s value.
+     * @return A [CallbackToken] that identifies [callback] by it's origin, allowing it to be
+     * retrieved and invoked later.
+     */
     fun register(
         lifecycle: Lifecycle,
         runWhileStopped: Boolean = false,
