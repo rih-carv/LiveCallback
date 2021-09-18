02bd3503e375026a96d0892186b8c2709139460e
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-09-18 20:56:23 -0300

    SampleActivity structure

--- sample/src/main/java/com/ricarvalho/livecallback/SampleActivity.kt
+++ sample/src/main/java/com/ricarvalho/livecallback/SampleActivity.kt
@@ -2,10 +2,14 @@ package com.ricarvalho.livecallback
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.ricarvalho.livecallback.databinding.ActivitySampleBinding
 
 class SampleActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySampleBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sample)
+        binding = ActivitySampleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
