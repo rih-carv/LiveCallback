849294d2811c47f46464f8c87f3f890bda1156fd
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-09-18 21:01:28 -0300

    Samples navigation implementation

--- sample/src/main/java/com/ricarvalho/livecallback/SampleActivity.kt
+++ sample/src/main/java/com/ricarvalho/livecallback/SampleActivity.kt
@@ -2,6 +2,7 @@ package com.ricarvalho.livecallback
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.ricarvalho.livecallback.databinding.ActivitySampleBinding
 
 class SampleActivity : AppCompatActivity() {
@@ -11,5 +12,13 @@ class SampleActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySampleBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupView()
+    }
+
+    private fun setupView() {
+        binding.viewPager.adapter = SectionsAdapter(this)
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = resources.getStringArray(R.array.tab_names).getOrNull(position)
+        }.attach()
     }
 }
