56eaddd318a8880487a18badfbef639630e071f0
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-09-18 21:07:00 -0300

    Coroutines dependencies fix

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -38,9 +38,10 @@ dependencies {
     implementation(project(":livecallback"))
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("com.google.android.material:material:1.4.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.5.2")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
