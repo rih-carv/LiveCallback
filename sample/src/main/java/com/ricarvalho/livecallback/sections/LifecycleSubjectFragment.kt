dab71e1a3fad1d57f24f7485ff7c1af66eb1febb
Ricardo Carvalho <ricardo.tks.best@gmail.com>
2021-09-19 22:59:12 -0300

    Lifecycle sample subject fragment implementation

--- sample/src/main/java/com/ricarvalho/livecallback/sections/LifecycleSubjectFragment.kt
+++ sample/src/main/java/com/ricarvalho/livecallback/sections/LifecycleSubjectFragment.kt
@@ -4,12 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.ricarvalho.livecallback.InputCallbackToken
+import com.ricarvalho.livecallback.R
 import com.ricarvalho.livecallback.databinding.FragmentLifecycleSubjectBinding
+import com.ricarvalho.livecallback.sdk.LegacySdkWrapper
+import com.ricarvalho.livecallback.sdk.LegacySdkWrapper.representation
 
 class LifecycleSubjectFragment : Fragment() {
-    private val callbackToken: InputCallbackToken<Result<String>>? = null
+    private var onlyWhenActiveCallbackToken: InputCallbackToken<Result<String>>? = null
+    private var alsoWhenStoppedCallbackToken: InputCallbackToken<Result<String>>? = null
     private var binding: FragmentLifecycleSubjectBinding? = null
 
     override fun onCreateView(
@@ -31,7 +36,30 @@ class LifecycleSubjectFragment : Fragment() {
         binding = null
     }
 
-    private fun registerCallbacks() {}
+    private fun registerCallbacks() {
+        onlyWhenActiveCallbackToken = LegacySdkWrapper.registry.register(
+            viewLifecycleOwner.lifecycle,
+            callback = this::updateResultText
+        )
+        alsoWhenStoppedCallbackToken = LegacySdkWrapper.registry.register(lifecycle, true) {
+            Toast.makeText(context, it.representation, Toast.LENGTH_SHORT).show()
+        }
+    }
 
-    private fun setupView() {}
+    private fun updateResultText(result: Result<String>?) {
+        binding?.resultTextView?.text = getString(R.string.result, result?.representation ?: "")
+    }
+
+    private fun setupView() = binding?.apply {
+        val instance = Integer.toHexString(this@LifecycleSubjectFragment.hashCode()).uppercase()
+        instanceTextView.text = getString(R.string.instance, instance)
+        updateResultText(null)
+        triggerButton.setOnClickListener {
+            LegacySdkWrapper.perform(
+                onlyWhenActiveCallbackToken ?: return@setOnClickListener,
+                alsoWhenStoppedCallbackToken ?: return@setOnClickListener,
+                customMessage = "invoked from fragment instance 0x$instance"
+            )
+        }
+    }
 }
